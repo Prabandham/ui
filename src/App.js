7be7f324019833bf2c27498112abe7e1026ab0fd
Srinidhi <srinidhi.prabandham@sycamoreinformatics.com>
2020-04-24 13:15:20 +0530

    Building base design for landing page

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+  Link,
+  Route,
+  BrowserRouter as Router,
+  Switch,
+} from 'react-router-dom';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import LandinPageContainer from './containers/LandingPageContainer';
+
+export default class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="container-fluid">
+          <div className="float-right mb-5">
+            <Link to="/login" exact="true" className="btn btn-link">Login</Link>{' '}
+          </div>
+          <br />
+          <br />
+          <br />
+
+          <Switch>
+            <Route path="/" component={LandinPageContainer} />
+            <Route path="/about" component={LandinPageContainer} />
+            <Route path="/contact" render={() => <h1>Contact Us</h1>} />
+            <Route render={() => <h1>Page not found</h1>} />
+          </Switch>
+        </div>
+      </Router>
+    )
+  }
 }
-
-export default App;
---
+++ src/containers/LandingPageContainer.js
@@ -0,0 +1,40 @@
+import { Button, Col, Row } from "reactstrap"
+import React, { Component } from 'react'
+
+export default class LandinPageContainer extends Component {
+    state = {
+        isLoggedIn: false,
+    }
+
+    componentDidMount() {
+    }
+
+    render() {
+        return (
+            <Row>
+                <Col xs="3" className="mt-5">
+                    <h1 className="mt-5">
+                        <span className="text-warning">Cost </span>
+                        <span className="text-info" style={{ "color": "#2c458b" }}>Tracker</span>
+                    </h1>
+                    <p>
+                        An effective and simple cost tracking application.
+                        Which does all the work, with cutting edge technology.
+                    </p>
+                    <p>
+                        Track all your expenses and analyse with,
+                        Visual Tools that lets you dive into your spedning habbits thus helping you save more.
+                    </p>
+                    <br />
+                    <Button color="warning">Sign up</Button>
+                </Col>
+                <Col xs="9">
+                    <div className="text-center">
+                        <img src="/images/landing_page_bg.jpg" alt="landing-page-bg" className="img-fluid"></img>
+                    </div>
+
+                </Col>
+            </Row>
+        )
+    }
+}
